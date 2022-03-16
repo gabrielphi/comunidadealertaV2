d72a190420249063c3c3df6fb758b56d4c5989ec
Gabriel Laureth Philippi <gabriellaurethphilippi@gmail.com>
2022-03-16 16:53:14 -0300

    configMysql

--- comunidadealertaV2/settings.py
+++ comunidadealertaV2/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'comunidadealertaV2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
+        'NAME'    : 'comunidadeAlerta',                 # <-- UPDATED line 
+        'USER'    : 'user',                     # <-- UPDATED line
+        'PASSWORD': 'senha123',              # <-- UPDATED line
+        'HOST'    : 'localhost',                # <-- UPDATED line
+        'PORT'    : '3306',
     }
 }
 

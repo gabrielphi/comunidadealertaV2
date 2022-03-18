12a2f6ee89a3a6db962da604dc82463f222e3139
Gabriel Laureth Philippi <gabriellaurethphilippi@gmail.com>
2022-03-17 22:23:49 -0300

    reformulacao

--- django/comunidadealertaV2/settings.py
+++ django/comunidadealertaV2/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/django/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'django')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
